1e6a58698c87decaf69fae22fc865720fdd89f28
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-03 14:58:52 -0400

    expands functional test for a user's first visit

--- superlists/func-tests.py
+++ superlists/func-tests.py
@@ -1,5 +1,7 @@
 import unittest 
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest( unittest.TestCase ) : 
@@ -27,15 +29,30 @@ class NewVisitorTest( unittest.TestCase ) :
 
         # She notices the page title and header mention to-do lists
         self.assertIn( 'To-Do' , self.browser.title )
-
-        self.fail( 'Finish writing the test!' )
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn( 'To-Do' , header_text )
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id( 'add-new-item' )
+        self.assertEqual( 
+            inputbox.get_attribute( 'placeholder' ) , 
+            'Enter a to-do item' 
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        first_item_text = 'Buy peacock feathers' 
+        inputbox.send_keys( first_item_text )
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys( Keys.ENTER )
+
+        table = self.browser.find_element_by_id( 'list-table' )
+        rows = table.find_elements_by_tag_name( 'tr' )
+        self.assertTrue( 
+            any( row.text == '1: ' + first_item_text for row in rows ) , 
+            'Could not find "' + first_item_text + '" in the table.'
+        )
 
         # There is still a text box inviting her to add another item. 
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -49,6 +66,9 @@ class NewVisitorTest( unittest.TestCase ) :
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+
+        self.fail( 'Finish writing the test!' )
+
 
 # run the test
 if __name__ == '__main__' : 
