712dd4ca383030c02766558c6b8b44bc5dcb75b4
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-03 13:46:50 -0400

    adds first test :: url mapping :: dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
+
+class DummyTest( TestCase ) : 
+
+    def test_root_url_resolves_to_home_page( self ) :
+
+        page_found = resolve( '/' )
+        self.assertEqual( page_found.func , home_page ) 
+
+
+
+# don't forget about templateUsed()
+
