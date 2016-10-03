0c359e9c9cb2463bb0551ef01713df7ed0fe5177
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-03 14:31:36 -0400

    writes html test :: passes test with minimal html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from lists.views import home_page
+from django.http import HttpRequest
 
+from lists.views import home_page
 
 class DummyTest( TestCase ) : 
 
@@ -11,6 +12,13 @@ class DummyTest( TestCase ) :
         self.assertEqual( page_found.func , home_page ) 
 
 
+    def test_home_page_returns_correct_html( self ) :
+
+        request = HttpRequest() 
+        response = home_page( request )
+        self.assertTrue( response.content.startswith( b'<html>' ))
+        self.assertIn( b'<title>To-Do Lists</title>' , response.content )
+        self.assertTrue( response.content.endswith( b'</html>' ))
 
 # don't forget about templateUsed()
 
