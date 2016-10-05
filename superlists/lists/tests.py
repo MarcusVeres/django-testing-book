06701a5b412c32e146bdfc1aa7b8c679833ddc3a
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-05 11:34:52 -0400

    adds model for list items + associated migrations + tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item 
 from lists.views import home_page
 
 class DummyTest( TestCase ) : 
@@ -44,4 +45,32 @@ class DummyTest( TestCase ) :
         self.assertEqual( response.status_code , 200)
         self.assertContains( response , item_text );
         # self.assertEqual( response.content.decode() , expected_html )
+
+
+class ItemModelTest( TestCase ) : 
+    
+    def test_saving_and_retrieving_items( self ) : 
+
+        first_item = Item() 
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        third_item = Item()
+        third_item.text = 'The third item'
+        third_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual( saved_items.count() , 3 )
+
+        first_saved_item = saved_items[ 0 ] 
+        second_saved_item = saved_items[ 1 ] 
+        third_saved_item = saved_items[ 2 ] 
+
+        self.assertEqual( first_saved_item.text , 'The first (ever) list item' )
+        self.assertEqual( second_saved_item.text , 'The second item' )
+        self.assertEqual( third_saved_item.text , 'The third item' )
 
