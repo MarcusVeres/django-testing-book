63930fe15a2a82cbbb22265d9d8518e5197354b0
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-11 14:50:31 -0400

    renames DummyTest to the more-doscriptive HomePageTest

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 from lists.models import Item , List
 from lists.views import home_page
 
-class DummyTest( TestCase ) : 
+class HomePageTest( TestCase ) : 
 
     def test_root_url_resolves_to_home_page( self ) :
 
