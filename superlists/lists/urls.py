415f33ce1f58634390bc37bf7a0a0ea5b08f354c
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-11 17:34:39 -0400

    creates a URLs file for lists and moves lists-related URLs there :: removes lists-specific URLs from the main URLs file

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import include , url
+from django.conf.urls import url
 from django.contrib import admin
 from lists import views
 
+# (.+) will match any characters up to the following /
+# (\d+) will match any numbers up to the following /
+
 urlpatterns = [
-    url( r'^$' , views.home_page , name='home_page' ) , 
-    url( r'^lists/' , include( 'lists.urls' ) ),  
+    url( r'^(\d+)/$' , views.view_list , name='view_list' ) ,
+    url( r'^(\d+)/add_item$' , views.add_item , name='add_item' ) ,
+    url( r'^new' , views.new_list , name='new_list' ) ,
+    # url( r'^lists/(.+)/$' , views.view_list , name='view_list' ) ,
 ]
 
