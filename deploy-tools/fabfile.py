aaab0d395a50d6d9891dd8a9ade09d692a291e9b
Marcus Veres <marcus.veres@juicemobile.com>
2016-10-25 17:14:19 -0400

    adds an update_settings function to switch up settings for production :: renames 'sitename' to 'site_name' :: adds code to wipe server code and sync it with local code

--- deploy-tools/fabfile.py
+++ deploy-tools/fabfile.py
@@ -25,13 +25,13 @@ def remote() :
 
 @task
 def deploy() :
-    env.sitename = DEFAULT_SITE_NAME
+    env.site_name = DEFAULT_SITE_NAME
 
     # env.user is what you used to log into the server 
     # env.host is provided when you run the fab command from your CLI
 
     # define vars 
-    site_folder = '/home/%s/sites/www/%s' % ( env.user , env.sitename )
+    site_folder = '/home/%s/sites/www/%s' % ( env.user , env.site_name )
     source_folder = site_folder + '/source' 
 
     # execute deploy functions 
@@ -40,15 +40,15 @@ def deploy() :
     _create_main_folder_if_necessary( site_folder )
     _get_latest_source( site_folder , source_folder ) 
     _create_directory_structure_if_necessary( site_folder )
+    _update_settings( source_folder , env.site_name )
     return
-    _update_settings( source_folder , env.host ) 
     _update_virtualenv( source_folder ) 
     _update_static_files( source_folder ) 
     _update_database( source_folder ) 
 
 
 def _describe_self( site_folder ) : 
-    print "This script will deploy: %s \n hosted on %s \n as %s \n to %s\n" % ( env.sitename , REPO_URL , env.user , site_folder )
+    print "This script will deploy: %s \n hosted on %s \n as %s \n to %s\n" % ( env.site_name , REPO_URL , env.user , site_folder )
 
 
 def _get_system_information():
@@ -64,31 +64,51 @@ def _get_latest_source( site_folder , source_folder ) :
 
     # check if the git repo already exists 
     if exists( site_folder + '/.git' ) : 
-        print "there is a git repo here" 
+        print "A Git repo for this project already exists. Fetching..." 
+        run( 'cd %s && git fetch' % ( source_folder ) ) 
 
     else : 
-        print "the git repo does not exist"
+        print "Did not find a Git repo at this location. Cloning..."
         run( 'git clone %s %s' % ( REPO_URL , site_folder ) )
+        #run( 'mv %s/superlists %s/source' % ( site_folder , site_folder ) ) << not necessary because it was renamed in the repo
 
-    return
+    # grab the git log hash from the LOCAL (dev) machine 
+    current_commit = local( "git log -n 1 --format=%H" , capture = True )
 
-
-#    if exists( site_folder + '/.git' ) : 
-#        run( 'cd %s && git fetch' % ( source_folder ) ) 
-#
-#    else : 
-#        run( 'git clone %s %s' % ( REPO_URL , site_folder ) 
-
-    # remember: your shit is a bit different than what he's done
-    # chdir to site_folder 
-    # run git pull into site_folder  
-    # rename superlists folder to "source" 
+    # hard reset to the current_commit, which will wipe away any current changes to the server's code
+    run( 'cd %s && git reset --hard %s' % ( source_folder , current_commit ) )
 
 
 def _create_directory_structure_if_necessary( site_folder ) : 
-    for subfolder in ( 'database' , 'static' , 'venv' , 'source' ) : 
+    for subfolder in ( 'database' , 'static' , 'venv' ) : 
         run( 'mkdir -p %s/%s' % ( site_folder , subfolder ) ) 
+
+
+def _update_settings( source_folder , site_name ) :
+    # set the ALLOWED_HOSTS and DEBUG, and create a new secret key
+
+    settings_file = source_folder + '/superlists/settings.py'
+
+    # find and replace the line with the DEBUG setting 
+    print "\nDisabling DEBUG mode..."
+    sed( settings_file , 'DEBUG = True' , 'DEBUG = False' )
+
+    # find the ALLOWED_HOSTS line, and add our site name to the list of allowed hosts
+    print "\nAdding %s to ALLOWED_HOSTS..." % ( site_name )
+    sed( settings_file , 
+        'ALLOWED_HOSTS = .+$' , 
+        'ALLOWED_HOSTS = ["%s"]' % ( site_name )
+        )
+
+    # generate a secret key
+    character_pool = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    new_key = '' . join( random.SystemRandom().choice( character_pool ) for _ in range( 50 ) ) 
+
+    # find and replace the secret key line
+    print "\nUpdating secret key..."
+    sed( settings_file , 'SECRET_KEY = .+$' , 'SECRET_KEY = "%s"' % ( new_key ) )
 
 
 # syntax : fab function_name:host=hostname.com
 # example : fab deploy:host=dummy.com
+
